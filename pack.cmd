bd9327646b9542834a14db227044f1b30b51a7ee
Roman Kharitonov <rkharito@yandex.ru>
2025-01-03 16:38:11 +1000

    Setup pak-building toolchain (2)

--- pack.cmd
+++ pack.cmd
@@ -1,22 +1,32 @@
 @echo off
 
+set BUILD_DIR=%~dp0\build
+set DIVINE=%~dp0\Tools\Divine\Divine.exe
+
 pushd %~dp0
 
 echo Settig up build directory...
-rmdir /s /q build
-mkdir build
-robocopy /S %~dp0\src %~dp0\build >nul
+rmdir /s /q %BUILD_DIR%
+mkdir %BUILD_DIR%
+robocopy /S %~dp0\src\Data %BUILD_DIR% >nul
 
-echo Packing localization XML...
-set LSX_DIR=%~dp0\build\Mods\Epic_Encounters_071a986c-9bfa-425e-ac72-7e26177c08f6\Localization
-.\Tools\Divine.exe -g dos2de -a convert-resources -i lsx -o lsb -s %LSX_DIR% -d %LSX_DIR%
+echo Compressing localization XML...
+set LSX_DIR=%BUILD_DIR%\Mods\Epic_Encounters_071a986c-9bfa-425e-ac72-7e26177c08f6\Localization
+%DIVINE% -g dos2de -a convert-resources -i lsx -o lsb -s %LSX_DIR% -d %LSX_DIR%
 pushd %LSX_DIR%
+del /s /q *.lsx >nul
+popd
+
+echo Compressing root templates XML...
+set RT_DIR=%BUILD_DIR%\Public\Epic_Encounters_071a986c-9bfa-425e-ac72-7e26177c08f6\RootTemplates
+%DIVINE% -g dos2de -a convert-resources -i lsx -o lsb -s %RT_DIR% -d %RT_DIR%
+pushd %RT_DIR%
 del /s /q *.lsx >nul
 popd
 
 echo Building PAK...
 rmdir /s /q output
 mkdir output
-.\Tools\Divine.exe -g dos2de -a create-package -c lz4hc -s %~dp0\build -d %~dp0\output\EpicEncounters.pak
+%DIVINE% -g dos2de -a create-package -c lz4hc -s %BUILD_DIR% -d %~dp0\output\EpicEncounters.pak
 
 popd
---
+++ publish.cmd
@@ -0,0 +1,2 @@
+@echo off
+xcopy /Y %~dp0\output\*.pak "%USERPROFILE%\Documents\Larian Studios\Divinity Original Sin Enhanced Edition\Mods\"
