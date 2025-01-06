32d85461e59f1673f9e2f9e075ef5cca712bed08
Roman Kharitonov <rkharito@yandex.ru>
2025-01-06 22:04:13 +1000

    Allow to build vanilla mod without new crafting recipes, add readme to build

--- pack.cmd
+++ pack.cmd
@@ -1,5 +1,12 @@
 @echo off
 
+set PAK_NAME=EpicEncountersRu.pak
+set WITH_ADDITIONAL_COMBOS=no
+if [%1] == [--custom] (
+    set PAK_NAME=EpicEncountersRu_CustomRefaimVersion.pak
+    set WITH_ADDITIONAL_COMBOS=yes
+)
+
 set SRC_DIR=%~dp0\src
 set BUILD_DIR=%~dp0\build
 set TOOLS_DIR=%~dp0\tools
@@ -14,6 +21,11 @@ echo Settig up build directory...
 rmdir /s /q %BUILD_DIR%
 mkdir %BUILD_DIR%
 robocopy /S %SRC_DIR%\Data %BUILD_DIR% >nul
+
+if %WITH_ADDITIONAL_COMBOS% == yes (
+    echo Incorporating additional item combos...
+    xcopy /y %SRC_DIR%\CustomItemCombos\*.* %BUILD_DIR%\Public\Epic_Encounters_071a986c-9bfa-425e-ac72-7e26177c08f6\Stats >nul
+)
 
 echo Compressing localization XML...
 set LSX_DIR=%BUILD_DIR%\Mods\Epic_Encounters_071a986c-9bfa-425e-ac72-7e26177c08f6\Localization
@@ -32,6 +44,7 @@ popd
 echo Building PAK...
 rmdir /s /q output
 mkdir output
-%DIVINE% -g dos2de -a create-package -c lz4hc -s %BUILD_DIR% -d %~dp0\output\EpicEncounters.pak
+%DIVINE% -g dos2de -a create-package -c lz4hc -s %BUILD_DIR% -d %~dp0\output\%PAK_NAME%
+copy /y %SRC_DIR%\readme.txt %~dp0\output\EpicEncountersReadme.txt >nul
 
 popd
